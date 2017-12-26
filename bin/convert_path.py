c4e20cf142f620010175dcee4d59ac8bb113f910
Pavel Podkorytov <pod.pavel@gmail.com>
2017-12-26 17:29:58 +0500

    Add convert_path tool

---
+++ bin/convert_path.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import argparse
+
+from pymetatile import Tile, Metatile
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Convert filepath between tile and metatile.")
+    parser.add_argument("-d", "--basedir", default="", help="output basedir prefix")
+    parser.add_argument("-e", "--ext", help="output extension"),
+    parser.add_argument("PATH", nargs="+", help="intput filepath")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    for path in args.PATH:
+        if path.endswith(".meta"):
+            mt = Metatile.from_url(path)
+            t = Tile.from_metatile(mt, ext=args.ext)
+            print(t.filepath(args.basedir))
+        else:
+            t = Tile.from_url(path)
+            mt = Metatile.from_tile(t)
+            print(mt.filepath(args.basedir))
+
+
+if __name__ == "__main__":
+    main()
