d9072ae860c897c8c60310888965b4afab318886
Pavel Podkorytov <pod.pavel@gmail.com>
2018-01-24 11:05:26 +0500

    Added Bound and Bounds objects

--- pyosm/point.py
+++ pyosm/point.py
@@ -41,3 +41,80 @@ def latlong_to_zxy(lat, lng, zoom):
     x = int((lng + 180.0) / 360.0 * n)
     y = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
     return ZXY(zoom, x, y)
+
+
+class Bound(object):
+    def __init__(self, z, min_x, max_x, min_y, max_y, flip_y=False):
+        self.z = z
+        self.min_x = min_x
+        self.max_x = max_x
+        if flip_y:
+            self.min_y = flip_y_coord(z, min_y)
+            self.max_y = flip_y_coord(z, max_y)
+        else:
+            self.min_y = min_y
+            self.max_y = max_y
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return "Bound(z:{} x:{}-{} y:{}-{})".format(self.z,
+                                                    self.min_x, self.max_x,
+                                                    self.min_y, self.max_y)
+
+    def __contains__(self, item):
+        """
+        >>> bound = Bound(z=15, min_x=26248, max_x=26253, min_y=10821, max_y=10816)
+        >>> print(ZXY(z=15, x=26248, y=10821) in bound)
+        True
+        >>> print(ZXY(z=15, x=26249, y=10822) in bound)
+        True
+        >>> print(ZXY(z=15, x=26247, y=10820) in bound)
+        False
+        """
+
+        if item.z != self.z:
+            return False
+
+        if item.x < self.min_x and item.x > self.max_x:
+            return False
+
+        if item.y < self.min_y and item.y > self.max_y:
+            return False
+
+        return True
+
+class Bounds(object):
+    def __init__(self, bounds):
+        self.bounds = bounds
+
+    def __str__(self):
+        return str(self.bounds)
+
+    def for_zoom(self, z):
+        for bound in self.bounds:
+            if bound.z == z:
+                return bound
+
+        raise ValueError("bound for given zoom not found")
+
+    def __contains__(self, item):
+        """
+        >>> bounds = Bounds([
+        ...     Bound(z=12, min_x=3281, max_x=3281, min_y=1352, max_y=1352),
+        ...     Bound(z=15, min_x=26248, max_x=26253, min_y=10821, max_y=10816),
+        ... ])
+        >>> print(ZXY(z=12, x=3281, y=1352) in bounds)
+        True
+        >>> print(ZXY(z=15, x=26249, y=10822) in bounds)
+        True
+        >>> print(ZXY(z=15, x=26247, y=10820) in bounds)
+        False
+        """
+
+        for bound in self.bounds:
+            if item in bound:
+                return True
+
+        return False
