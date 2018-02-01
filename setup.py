256a0f41a28a29a202584b2b8840c8dfd4206068
Pavel Podkorytov <pod.pavel@gmail.com>
2018-02-01 15:18:06 +0500

    Bump version to v0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyosm",
-    version="0.5",
+    version="0.6",
     description="Library for building OSM tools",
     url="https://github.com/tierpod/pyosm",
     author="Pavel Podkorytov",
